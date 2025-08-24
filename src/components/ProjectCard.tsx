0c85d6aa27442852044b2b0e6aab493c22e84c2a
miv986 <miros-98@hotmail.es>
2025-08-24 19:13:48 +0200

    feature/add-favicon and fix style issue

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -25,7 +25,7 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
         <motion.img
           src={project.image}
           alt={project.title[currentLang]}
-          className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
+          className="w-full h-full object-contain transition-transform duration-500 group-hover:scale-110"
           whileHover={{ scale: 1.05 }}
         />
         <div className="absolute inset-0 bg-gradient-to-t from-raisin-black/20 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
