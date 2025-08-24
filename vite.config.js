25d43af2d19d5a1cbf9184dc0ce05b85a8d4a239
miv986 <miros-98@hotmail.es>
2025-08-24 21:19:10 +0200

    Config build and deploy

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/portfolio-page/',
-  plugins: [react()],
   build: {
-    outDir: 'dist',  // <-- aquí cambias de dist a docs
+    outDir: 'docs',  // <-- aquí cambias de dist a docs
   },
   server: {
     host: 'localhost', // <-- clave
