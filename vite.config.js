90a867de9ae0292b39f6649f5a73d8e1d9b7d260
miv986 <miros-98@hotmail.es>
2025-08-24 20:25:43 +0200

    Configurar build y deploy para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/portfolio-page/',
+export default defineConfig(({ command, mode }) => ({
+  base: command === 'build' && mode === 'github'
+    ? '/portfolio-page/'  // para GitHub Pages
+    : '/',                // para Netlify y local
   plugins: [react()],
-  server: {
-    host: 'localhost', // <-- clave
-    port: 5173,         // opcional, puedes cambiarlo si el puerto está ocupado
-    strictPort: true    // lanza error si el puerto está ocupado (útil para depurar)
-  }
-})
+}))
