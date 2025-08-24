f2a81317d50ef974d91eefb40831b6eb61849619
miv986 <miros-98@hotmail.es>
2025-08-24 21:16:28 +0200

    Config build and deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/portfolio-page/',
+  plugins: [react()],
+  build: {
+    outDir: 'dist',  // <-- aquí cambias de dist a docs
+  },
   server: {
     host: 'localhost', // <-- clave
     port: 5173,         // opcional, puedes cambiarlo si el puerto está ocupado
